cc5b760f484d284e211f3234b29adada7c05cbb5
ThuPham02 <63948923+anhthu106@users.noreply.github.com>
2022-11-15 12:28:18 +0700

    remove unused function

--- src/backend/helper/items/items.js
+++ src/backend/helper/items/items.js
@@ -1,4 +1,4 @@
-import {fetchData} from "../fetchData/fetchData";
+import { fetchData } from "../fetchData/fetchData";
 
 async function returnMessage(res, setMessage) {
     let data = await res.json()
@@ -26,21 +26,11 @@ export async function deleteItems(body, e, setMessage, url) {
     await returnMessage(res, setMessage)
 }
 
-export function searchUsername(query, data){
+export function searchUsername(query, data) {
     //Our search filter function
     const searchFilter = (array) => {
         return array.filter(
             (el) => el.username.toLowerCase().includes(query.toLowerCase())
-        )
-    }
-    return searchFilter(data)
-}
-
-export function searchName(query, data) {
-    //Our search filter function
-    const searchFilter = (array) => {
-        return array.filter(
-            (el) => el.name.toLowerCase().includes(query.toLowerCase())
         )
     }
     return searchFilter(data)
