c23fa8359cd341aa31a7ac2fafbd5e4e5d044321
KN-1111 <106130549+KN2222@users.noreply.github.com>
2022-11-29 08:58:54 +0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -119,7 +119,6 @@ export default function Home({ courseProps, postProps }) {
                 <div className="flex-grow h-0 overflow-autos">
                   <h3 className="mt-6 font-semibold">Your Post</h3>
                   <div className="w-full">
-                    
                     <div className="flex w-full py-4 border-b border-gray-300">
                       <span className="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full"></span>
                       <div className="flex flex-col flex-grow ml-2">
@@ -127,6 +126,7 @@ export default function Home({ courseProps, postProps }) {
                           <span className="font-semibold">Username</span>
                           <span className="ml-1">@username</span>
                         </div>
+
                         <p className="mt-1 text-sm">
                           Lorem ipsum dolor sit amet, consectetur adipiscing
                           elit, et dolore magna aliqua.{" "}
