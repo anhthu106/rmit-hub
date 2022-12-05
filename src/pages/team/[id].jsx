9b41555de2d1c8efe178e68f833c200a3d538998
Dong <phamvodong0811@gmail.com>
2022-12-05 17:43:13 +0700

    Moveable task managment
    Task can be move but not save now

--- src/pages/team/[id].jsx
+++ src/pages/team/[id].jsx
@@ -1,31 +1,21 @@
-// BACKEND
-import { useState } from "react";
-import { useSession } from "next-auth/react";
+import {useState} from "react";
+import {DragDropContext, Draggable, Droppable, resetServerContext} from "react-beautiful-dnd";
 import connectDB from "../../backend/lib/connectDB";
-import importRawData from "../../backend/helper/data/data";
-import { deleteItems } from "../../backend/helper/items/items";
-
-// model
 import Course from "../../backend/models/course";
-import Users from "../../backend/models/user";
 import Teams from "../../backend/models/team";
 import List from "../../backend/models/list";
 import Task from "../../backend/models/task";
+import importRawData from "../../backend/helper/data/data";
+import Users from "../../backend/models/user";
 
-// COMPONENT
-import TeamInformation from "../../components/team/TeamInformation";
-import EditTeam from "../../components/team/EditTeam";
-import Button from "../../components/button/Button";
-import CreateList from "../../components/workspace/CreateList"
-import DisplayList from "../../components/workspace/DisplayList"
 
-export async function getServerSideProps({ params }) {
+export async function getServerSideProps({params}) {
     await connectDB()
 
     const courseData = await Course.find({}, "name")
     const teamData = await Teams.findById(params.id)
     const teamCourse = await Course.findById(teamData.courseID.toString(), "name")
-    const listData = await List.find({ team_id: params.id }, '_id title task_id team_id').populate('task_id', '_id description username createdDate deadline', Task)
+    const listData = await List.find({team_id: params.id}, '_id title task_id team_id').populate('task_id', '_id description username createdDate deadline', Task)
     const list = importRawData(listData, ['_id', 'team_id'], null)
 
     const lists = await Promise.all(
@@ -37,7 +27,6 @@ export async function getServerSideProps({ params }) {
         })
     )
 
-    console.log(lists)
     const courses = importRawData(courseData, ['_id'], null)
 
     const userId = teamData.userID.map((data) => {
@@ -59,6 +48,7 @@ export async function getServerSideProps({ params }) {
         userId: userId,
         user: userName
     }
+    resetServerContext();
 
     return {
         props: {
@@ -70,74 +60,129 @@ export async function getServerSideProps({ params }) {
     }
 }
 
-export default function TeamDetail({ listProps, TeamInfo, courseProps, userName }) {
-    const { data: session } = useSession()
-    const id = session.user._id
-    const [message, setMessage] = useState(null)
-    if (id === TeamInfo.userId[0]) {        //Login user
-        return (
-            <div>
-                {/*Display Team*/}
-                <TeamInformation
-                    Description={TeamInfo.description}
-                    Name={TeamInfo.name}
-                    Member={TeamInfo.members}
-                    CourseId={TeamInfo.courseName}
-                    User={TeamInfo.user}
-                />
+export default function Test({listProps, TeamInfo, courseProps, userName}) {
 
-                <EditTeam
-                    courseProps={courseProps}
-                    preCourse={TeamInfo.courseName}
-                    preName={TeamInfo.name}
-                    id={TeamInfo._id}
-                    preDescription={TeamInfo.description}
-                />
+    const onDragEnd = (result, columns, setColumns) => {
+        if (!result.destination) return;
 
-                {/*Working area*/}
-                <h2>Create list</h2>
-                <CreateList teamID={TeamInfo._id} />
-                {listProps.map((list) => (
-                    <div key={list._id}>
-                        <DisplayList
-                            listID={list._id}
-                            usernameProps={userName}
-                            taskProps={list}
-                        />
-                    </div>
-                ))}
-            </div>
-        )
-    } else {
-        return (
+        const { source, destination } = result;
 
-            <div>
-                <TeamInformation
-                    Description={TeamInfo.description}
-                    Name={TeamInfo.name}
-                    Member={TeamInfo.members}
-                    CourseId={TeamInfo.courseName}
-                    User={TeamInfo.user}
-                />
-                <h2>------Create list--------</h2>
-                <CreateList teamID={TeamInfo._id} />
-                {listProps.map((list) => (
-                    <div key={list._id}>
-                        <DisplayList
-                            taskProps={list}
-                            listID={list._id}
-                            usernameProps={userName}
-                        />
-                    </div>
-                ))}
-                <Button
-                    fn={(e) => deleteItems({ userId: id }, e, setMessage, `/api/team/${TeamInfo._id}`)}
-                    options={"Out Team"}
-                />
-                <div>{message}</div>
-            </div>
-        )
-    }
+        if (source.droppableId !== destination.droppableId) {
+
+            const sourceColumn = columns[source.index];
+            const destColumn = columns[destination.index];
+
+            const sourceItems = [...sourceColumn.task_id];
+            const destItems = [...destColumn.task_id];
+
+            const [removed] = sourceItems.splice(source.index, 1);
+
+            destItems.splice(destination.index, 0, removed);
+            setColumns({
+                ...columns,
+                [source.index]: {
+                    ...sourceColumn,
+                    task_id: sourceItems
+                },
+                [destination.index]: {
+                    ...destColumn,
+                    task_id: destItems
+                }
+            });
+        } else {
+            const column = columns[source.index];
+            const copiedItems = [...column.task_id];
+            const [removed] = copiedItems.splice(source.index, 1);
+            copiedItems.splice(destination.index, 0, removed);
+            setColumns({
+                ...columns,
+                [source.index]: {
+                    ...column,
+                    task_id: copiedItems
+                }
+            });
+        }
+    };
+    const [columns, setColumns] = useState(listProps);
+    console.log(columns)
+    return (
+        <div style={{display: "flex", justifyContent: "center", height: "100%"}}>
+            <DragDropContext
+                onDragEnd={result => onDragEnd(result, columns, setColumns)}
+            >
+                {columns.map((column) => {
+                    return (
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center"
+                            }}
+                            key={column._id}
+                        >
+                            <h2>{column.title}</h2>
+                            <div style={{margin: 8}}>
+                                <Droppable droppableId={column._id} key={column._id}>
+                                    {(provided, snapshot) => {
+                                        return (
+                                            <div
+                                                {...provided.droppableProps}
+                                                ref={provided.innerRef}
+                                                style={{
+                                                    background: snapshot.isDraggingOver
+                                                        ? "lightblue"
+                                                        : "lightgrey",
+                                                    padding: 4,
+                                                    width: 250,
+                                                    minHeight: 500
+                                                }}
+                                            >
+                                                {column.task_id.map((task, index) => {
+                                                    return (
+                                                        <Draggable
+                                                            key={task._id}
+                                                            draggableId={task._id}
+                                                            index={index}
+                                                        >
+                                                            {(provided, snapshot) => {
+                                                                return (
+                                                                    <div
+                                                                        ref={provided.innerRef}
+                                                                        {...provided.draggableProps}
+                                                                        {...provided.dragHandleProps}
+                                                                        style={{
+                                                                            userSelect: "none",
+                                                                            padding: 16,
+                                                                            margin: "0 0 8px 0",
+                                                                            minHeight: "50px",
+                                                                            backgroundColor: snapshot.isDragging
+                                                                                ? "#263B4A"
+                                                                                : "#456C86",
+                                                                            color: "white",
+                                                                            ...provided.draggableProps.style
+                                                                        }}
+                                                                    >
+                                                                        {task.description}
+                                                                    </div>
+                                                                )
+                                                            }}
+                                                        </Draggable>
+                                                    )
+                                                })}
+                                                {provided.placeholder}
+                                            </div>
+                                        )
+                                    }
+                                    }
+                                </Droppable>
+                            </div>
+
+
+                        </div>
+                    )
+                })}
+
+            </DragDropContext>
+        </div>
+    )
 }
-
-TeamDetail.auth = true
