d91f77a33dca98caad029fc9c983e0ef3cff7f6b
Dong <phamvodong0811@gmail.com>
2022-12-07 22:48:57 +0700

    Clean code and comments

--- src/pages/api/socket/index.js
+++ src/pages/api/socket/index.js
@@ -4,19 +4,27 @@ import List from "../../../backend/models/list";
 
 export default async function handler(req, res) {
     await connectDB()
+
+    // Check socket exists
     if (res.socket.server.io) {
         console.log("Socket is already running")
 
     } else {
         console.log("Socket is initializing")
+        // Create new socket
         res.socket.server.io = new Server(res.socket.server)
+
         const io = res.socket.server.io
+
+        // When socket connected
         io.on("connection", socket => {
+            // List in roomList
             socket.on("List", (lists) => {
                 socket.join("roomList")
                 socket.broadcast.emit("MoveList", lists)
             })
 
+            // MongoUpdate in roomUpdate
             socket.on("MongoUpdate", (update) => {
                 socket.join("roomUpdate")
                 update.map(async (doc) => {
@@ -28,8 +36,6 @@ export default async function handler(req, res) {
                 })
             })
         })
-
-
     }
     res.end()
 }
