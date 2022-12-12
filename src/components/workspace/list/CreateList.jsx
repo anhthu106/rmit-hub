6780d5ea17bff0bea417ce88c95d5a0886292871
KN-1111 <106130549+KN2222@users.noreply.github.com>
2022-12-12 15:10:19 +0700

    board updated

--- src/components/workspace/list/CreateList.jsx
+++ src/components/workspace/list/CreateList.jsx
@@ -11,7 +11,7 @@ export default function CreateList({teamID}) {
         <div>
             <form>
                 <div>
-                    <label htmlFor="title">Title</label>
+                    <label htmlFor="title" class="block mb-2 text-sm font-medium text-gray-900">Title</label>
                     <input
                         placeholder="Enter list title..."
                         type="text"
@@ -20,9 +20,12 @@ export default function CreateList({teamID}) {
                         required
                         value={title}
                         onChange={e => setTitle(e.target.value)}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     />
                 </div>
-                <button onClick={(e) => {
+                <button 
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center"
+                onClick={(e) => {
                     const data = {title, teamID}
 
                     socket = io()
--- src/components/workspace/list/EditList.jsx
+++ src/components/workspace/list/EditList.jsx
@@ -1,40 +1,40 @@
-import {useEffect, useState} from "react";
-import {io} from "socket.io-client";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
-let socket
+let socket;
 
-export default function EditList({listTile, listId}) {
-    const [title, setTitle] = useState(listTile);
+export default function EditList({ listTile, listId }) {
+  const [title, setTitle] = useState(listTile);
 
-    useEffect(() => {
-        setTitle(listTile)
-    }, [listTile])
+  useEffect(() => {
+    setTitle(listTile);
+  }, [listTile]);
 
+  const onKeyDown = (e) => {
+    if (e.key === "Enter" || e.key === "Escape") {
+      const data = { title, listId };
 
-    const onKeyDown = (e) => {
-        if (e.key === "Enter" || e.key === "Escape") {
-            const data = {title, listId}
+      socket = io();
+      socket.emit("editList", data);
 
-            socket = io()
-            socket.emit("editList", data)
-
-            e.preventDefault()
-        }
+      e.preventDefault();
     }
-    return (
-        <div className="text-lg font-semibold mb-7">
-            <form>
-                <div>
-                    <input
-                        type="text"
-                        id="title"
-                        name="title"
-                        value={title}
-                        onChange={e => setTitle(e.target.value)}
-                        onKeyDown={onKeyDown}
-                    />
-                </div>
-            </form>
+  };
+  return (
+    <div className="text-lg font-semibold mb-7">
+      <form>
+        <div>
+          <input
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            onKeyDown={onKeyDown}
+          />
         </div>
-    )
+      </form>
+    </div>
+  );
 }
