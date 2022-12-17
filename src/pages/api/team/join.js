46fa15c1f5fb09783ce1dfc36e71efd59dc2ef27
ThuPham02 <63948923+anhthu106@users.noreply.github.com>
2022-12-17 12:48:14 +0700

    fix pending list

--- src/pages/api/team/join.js
+++ src/pages/api/team/join.js
@@ -7,36 +7,26 @@ import { StatusCodes } from "http-status-codes";
 export default async function handler(req, res) {
     await connectDB();
 
-    console.log(req.body)
-    //Take the user value
+    console.log(req.body);
+
     const leader = await Users.findById(req.body.leader, "email")
     const member = await Users.findById(req.body.currentUser, "username")
 
     const team = await Teams.findById(req.body.teamID, "pending");
 
-    let arr = []
-    if (team.pending.length == 0) {
-        arr.push(member._id)
-    }
-    for (let i of team.pending) {
-        if (!arr.includes(i)) {
-            arr.push(member._id)
+    if (!team.pending.includes(member._id)) {
+        await Teams.findByIdAndUpdate(req.body.teamID,
+            { $push: { pending: member._id } })
+        if (leader) {
+            const leaderEmail = leader.email
+            const memberName = member.username
+            const teamID = team._id
+            await requestToJoinTeam({ leaderEmail, memberName, teamID })
+
+            res.status(StatusCodes.OK).json({ message: "Verify email is sent which have 5 minutes expires" })
+        } else {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Invalid email or not found" })
         }
-    }
-
-    await Teams.findByIdAndUpdate(req.body.teamID, {
-        pending: arr
-    })
-
-    if (leader) {
-        const leaderEmail = leader.email
-        const memberName = member.username
-        const teamID = team._id
-        await requestToJoinTeam({ leaderEmail, memberName, teamID })
-
-        res.status(StatusCodes.OK).json({ message: "Verify email is sent which have 5 minutes expires" })
-    } else {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Invalid email or not found" })
     }
 
 }
