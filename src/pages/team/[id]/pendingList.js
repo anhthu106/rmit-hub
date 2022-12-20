b714eea63cd559899fa814d75120051509b3a60d
KN-1111 <106130549+KN2222@users.noreply.github.com>
2022-12-20 14:46:50 +0700

    error request check

--- src/pages/team/[id]/pendingList.js
+++ src/pages/team/[id]/pendingList.js
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 import importRawData from "../../../backend/helper/data/data";
 import { addItems } from "../../../backend/helper/items/items";
 import Image from "next/image";
+import Link from "next/link";
 
 import Teams from "../../../backend/models/team";
 import Users from "../../../backend/models/user";
@@ -125,43 +126,49 @@ export default function TeamDetail({ userProps, team }) {
           </div>
         ))} */}
 
-        <Header/>
-        
+        <Header />
+
         <section className="bg-white bg-gradient-to-tr from-blue-200 via-indigo-200 to-pink-200  min-h-[calc(100vh-62px)]">
           <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
-            <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div className="grid gap-8 lg:gap-16 md:grid-cols-2 lg:grid-cols-3">
               {userProps.map((user) => (
                 <div key={user._id}>
                   <div className="card w-96 mx-auto bg-white  shadow-xl hover:shadow">
-                    <div className="w-fit mx-auto pt-3">
-                      <Image
-                        key={user.image.imgURL}
-                        className="rounded-full"
-                        src={user.image.imgURL}
-                        alt="avatar"
-                        width="150"
-                        height="150"
-                      />
-                    </div>
+                    <Link href={`/users/${user._id}`}>
+                      <a>
+                        <div className="w-fit mx-auto pt-3">
+                          <Image
+                            key={user.image.imgURL}
+                            className="rounded-full"
+                            src={user.image.imgURL}
+                            alt="avatar"
+                            width="150"
+                            height="150"
+                          />
+                        </div>
 
-                    <div className="text-center mt-2 text-3xl font-medium">
-                      {user.username}
-                    </div>
-                    <div className="text-center mt-2 font-light text-sm">
-                      {user.email}
-                    </div>
-                    <div className="text-center mt-2 font-light text-sm">
-                      {user.campus}
-                    </div>
-                    <div className="px-6 text-center mt-2 font-light text-sm">
-                      <p>{user.major_id.name}</p>
-                    </div>
+                        <div className="text-center mt-2 text-3xl font-medium">
+                          {user.username}
+                        </div>
+                        <div className="text-center mt-2 font-light text-sm">
+                          {user.email}
+                        </div>
+                        <div className="text-center mt-2 font-light text-sm">
+                          {user.campus}
+                        </div>
+                        <div className="px-6 text-center mt-2 font-light text-sm">
+                          <p>{user.major_id.name}</p>
+                        </div>
+                      </a>
+                    </Link>
+
                     <hr className="mt-8" />
+
                     <div className="flex p-4 space-x-2">
                       <div className="w-1/2">
                         <Button
                           style="w-full bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-white text-center"
-                          fn={(e) =>
+                          fn={(e) => {
                             addItems(
                               {
                                 teamID: id,
@@ -172,8 +179,9 @@ export default function TeamDetail({ userProps, team }) {
                               e,
                               setMessage,
                               "/api/team/pending"
-                            )
-                          }
+                            );
+                            window.location.reload(false);
+                          }}
                           options={"Accept Request"}
                         />
                       </div>
@@ -181,7 +189,7 @@ export default function TeamDetail({ userProps, team }) {
                       <div className="w-1/2">
                         <Button
                           style="w-full focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5"
-                          fn={(e) =>
+                          fn={(e) => {
                             addItems(
                               {
                                 teamID: team,
@@ -192,8 +200,9 @@ export default function TeamDetail({ userProps, team }) {
                               e,
                               setMessage,
                               "/api/team/pending"
-                            )
-                          }
+                            );
+                            window.location.reload(false);
+                          }}
                           options={"Reject Request"}
                         />
                       </div>
