994c3d89440537615add6d5c6f8641fd9af550c2
ThuPham02 <63948923+anhthu106@users.noreply.github.com>
2022-12-27 00:32:18 +0700

    fix "After Deleted Post server give no message"

--- src/pages/api/posts/[id].js
+++ src/pages/api/posts/[id].js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
                     await User.findByIdAndUpdate(deletePost.userID, {$pull: {post_id: deletePost._id}});
                     await Team.findByIdAndUpdate(deletePost.teamID, {$pull: {postID: deletePost._id}})
-                    res.status(StatusCodes.OK).json({message: "Deleted"});
+                    res.status(StatusCodes.OK).json({message: "Post Deleted"});
                 } catch (error) {
                     console.log(error);
                     return error
