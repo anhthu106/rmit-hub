4279be25206d491a174329aaafda8dc3e7dde678
Dong <phamvodong0811@gmail.com>
2022-12-29 15:51:46 +0700

    Fix enter recoverForm and CreateList
    Make the add recoverForm can enter to submit and make the editList can create when out the focus

--- src/components/auth/NewPasswordForm.jsx
+++ src/components/auth/NewPasswordForm.jsx
@@ -104,9 +104,7 @@ const NewPasswordForm = ({email}) => {
         {formCompelete && (<Button
             type="button"
             style="w-full  bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-white text-center"
-            fn={(e) =>
-                addItems({password}, e, setMessage, `/api/auth/reset/${email}`)
-            }
+            fn={(e) => addItems({password}, e, setMessage, `/api/auth/reset/${email}`)}
             options={"Reset Password"}
         />)}
 
