8340c54af87679ae7104d36a490bce2786ebe489
KN-1111 <106130549+KN2222@users.noreply.github.com>
2022-12-30 16:27:46 +0700

    Add reload team[id]/Management
    Closes #38

--- src/pageComponents/team/TeamLeaderPage.jsx
+++ src/pageComponents/team/TeamLeaderPage.jsx
@@ -3,30 +3,37 @@ import EditTeam from "../../components/team/EditTeam";
 import DisplayTeamMembers from "../../components/team/DisplayTeamMembers";
 import PendingList from "../../components/team/PendingList";
 
-export default function TeamLeaderPage({ team, userPending, userProps, session }) {
-    return (
-        <div className="pt-6 px-4">
-            <div className="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
-                <div
-                    className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 2xl:col-span-2 min-w-fit ">
-                    <DisplayTeamInfo
-                        team={team}
-                        tag={
-                            <EditTeam
-                            team={team}
-                                preName={team.name}
-                                preDescription={team.Description}
-                                id={team.id}
-                            />
-                        }
-                    />
-                </div>
+export default function TeamLeaderPage({
+  team,
+  userPending,
+  userProps,
+  session,
+}) {
+  return (
+    <div className="pt-6 px-4">
+      <div className="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
+        <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 2xl:col-span-2 min-w-fit ">
+          <DisplayTeamInfo
+            team={team}
+            tag={
+              <EditTeam
+                team={team}
+                preName={team.name}
+                preDescription={team.Description}
+                id={team.id}
+              />
+            }
+          />
+        </div>
 
-                <DisplayTeamMembers team={team} userProps={userProps} session={session} />
-            </div>
+        <DisplayTeamMembers
+          team={team}
+          userProps={userProps}
+          session={session}
+        />
+      </div>
 
-            <PendingList team={team} userPending={userPending} />
-
-        </div>)
-
+      <PendingList team={team} userPending={userPending} />
+    </div>
+  );
 }
