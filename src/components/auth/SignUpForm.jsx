98a5185a9b5773a51b34a4c9061b1c5af09a826f
ThuPham02 <63948923+anhthu106@users.noreply.github.com>
2022-12-30 11:40:45 +0700

    auto assign the tail at signup form

--- src/components/auth/SignUpForm.jsx
+++ src/components/auth/SignUpForm.jsx
@@ -1,13 +1,13 @@
 import makeAnimated from "react-select/animated";
-import {util} from "../../utils/utils";
-import {useEffect, useState} from "react";
-import {validPassword} from "../regularexpression/Regex";
+import { util } from "../../utils/utils";
+import { useEffect, useState } from "react";
+import { validPassword } from "../regularexpression/Regex";
 import Select from "react-select";
-import {Button, DisabledButton} from "../button/Button";
-import {addItems} from "../../backend/helper/items/items";
-import {signIn} from "next-auth/react";
+import { Button, DisabledButton } from "../button/Button";
+import { addItems } from "../../backend/helper/items/items";
+import { signIn } from "next-auth/react";
 
-const SignUpForm = ({majorProps}) => {
+const SignUpForm = ({ majorProps }) => {
     //Use animated of react-select
     const animatedComponents = makeAnimated();
 
@@ -17,7 +17,7 @@ const SignUpForm = ({majorProps}) => {
 
     //UseState
     const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
+    const [sid, setSID] = useState("");
     const [password, setPassword] = useState("");
     const [campus, setCampus] = useState(campusOptions[0].label);
     const [major, setMajor] = useState(majorOptions[0].label);
@@ -41,6 +41,7 @@ const SignUpForm = ({majorProps}) => {
     const [usernameIsUsed, setUsernameIsUsed] = useState(false);
     const [accountIsCreated, setAccountIsCreated] = useState(false);
 
+    const email = sid + "@rmit.edu.vn"
     //Function
     let emailIsOK = false;
     let passwordIsOK = false;
@@ -68,17 +69,21 @@ const SignUpForm = ({majorProps}) => {
         }
     }
 
+    function endsWithNumber(str) {
+        return /[0-9]+$/.test(str);
+    }
+
     function checkEmail() {
         if (
-            email.length === 20 &&
-            email.endsWith("@rmit.edu.vn") &&
-            email.startsWith("s", 0)
+            sid.length === 8 &&
+            sid.startsWith("s", 0) &&
+            endsWithNumber(sid)
         ) {
             setEmailError(false);
             emailIsOK = true;
         } else {
             emailIsOK = false;
-            if (email === "") {
+            if (sid === "") {
                 setEmailError(false);
             } else {
                 setEmailError(true);
@@ -182,19 +187,23 @@ const SignUpForm = ({majorProps}) => {
                 >
                     Email
                 </label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    placeholder="s1234567@rmit.edu.com"
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value.toLocaleLowerCase())}
-                />
+                <div className="w-full flex flex-row">
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm focus:ring-primary-600 focus:border-primary-600 p-2.5 w-4/5 rounded-l-lg"
+                        placeholder="s1234567"
+                        required
+                        value={sid}
+                        onChange={(e) => setSID(e.target.value.toLocaleLowerCase())}
+                    />
+                    <div className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm p-2.5 rounded-r-lg">@rmit.edu.vn</div>
+                </div>
+
                 {emailError && (
                     <p className="mt-2 text-xs text-red-600">
-                        Your email should be an RMIT&aposs email (sID@rmit.edu.vn)
+                        Your sID must contain letter "s" and 7 numbers (eg.s1234567)
                     </p>
                 )}
                 {emailIsUsed && (
@@ -293,29 +302,34 @@ const SignUpForm = ({majorProps}) => {
                 </div>
             </div>
 
-            {formUncompelete && (
-                <DisabledButton
-                    type="button"
-                    style="w-full text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                    options={"Register"}
-                />
-            )}
-            {formCompelete && (
-                <Button
-                    type="button"
-                    style="w-full  bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-white text-center"
-                    fn={(e) => {
-                        addItems(
-                            {username, email, password, campus, major},
-                            e,
-                            setMessage,
-                            "/api/auth/register"
-                        );
-                        setShowModal(true);
-                    }}
-                    options={"Register"}
-                />
-            )}
+            {
+                formUncompelete && (
+                    <DisabledButton
+                        type="button"
+                        style="w-full text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                        options={"Register"}
+                    />
+                )
+            }
+            {
+
+                formCompelete && (
+                    <Button
+                        type="button"
+                        style="w-full  bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-white text-center"
+                        fn={(e) => {
+                            addItems(
+                                { username, email, password, campus, major },
+                                e,
+                                setMessage,
+                                "/api/auth/register"
+                            );
+                            setShowModal(true);
+                        }}
+                        options={"Register"}
+                    />
+                )
+            }
             <span className="mt-2 text-sm text-green-600">{message}</span>
 
             <p className="text-sm font-light text-gray-500 ">
@@ -329,7 +343,7 @@ const SignUpForm = ({majorProps}) => {
                     />
                 </a>
             </p>
-        </form>
+        </form >
     );
 };
 
